3a84182e472a44be50699ea8cade4924b9738ff2
apple <stella_gan@126.com>
2021-07-27 23:39:43 +0800

    3-3

---
+++ 3-3.py
@@ -0,0 +1,4 @@
+trans=['bike','new energy car','Honda motorcycle']
+print("I would like to own a "+trans[0]+".")
+print("I would like to own a "+trans[1]+".")
+print("I would like to own a "+trans[2]+".")
