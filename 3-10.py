84893f64da9cdddd6d3f2d50fbfd043346a8a1a3
apple <stella_gan@126.com>
2021-07-28 15:17:05 +0800

    1

---
+++ 3-10.py
@@ -0,0 +1,27 @@
+food=['ice-cream','apple','milk','chicken']
+print("Print the list:")
+print(food)
+
+print("Print the first element:")
+print(food[0])
+
+print("Modify the first element as another item:")
+food[0]="tea"
+print(food)
+
+print("Insert an item at first:")
+food.insert(0,'ice-cream')
+print(food)
+
+print("Delete the last element:")
+del food[-1]
+print(food)
+
+print("Sort the list forever:")
+food.sort()
+print(food)
+
+print("Reverse the list:")
+food.reverse()
+print(food)
+
