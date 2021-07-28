deba1a1a34806a85a3e72084eb57f35901bb61a3
apple <stella_gan@126.com>
2021-07-28 12:22:43 +0800

    3-5

---
+++ 3-5.py
@@ -0,0 +1,12 @@
+people=['mom','dad','sister']
+print("Previous guest lists:")
+print(people[0])
+print(people[1])
+print(people[2])
+print("\nDelete:")
+print(people[2])#无法赴约的人
+people[2]='brother'
+print("\nNew guest lists:")
+print(people[0])
+print(people[1])
+print(people[2])
