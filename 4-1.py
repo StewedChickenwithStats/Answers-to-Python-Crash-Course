1ea76398520d4f308ff60bdd2aca31986482082a
apple <stella_gan@126.com>
2021-07-30 14:32:38 +0800

    1

---
+++ 4-1.py
@@ -0,0 +1,6 @@
+pizzas=['pepperoni','pan','chicken']
+for pizza in pizzas:
+	print(pizza)
+for pizza in pizzas:
+	print("I like "+pizza)
+print("I really love pizza!")
---
+++ 4-2.py
@@ -0,0 +1,6 @@
+animals=['dog','horse','cow']
+for animal in animals:
+	print(animal)
+for animal in animals:
+	print("A "+animal+" has four legs")
+print("Any of these animals has four legs")
